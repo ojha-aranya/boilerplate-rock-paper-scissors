d3fa700cb9116205423f38dc2fad4fa41739e09c
Aranya Ojha <identityanonymus6@gmail.com>
2025-08-20 05:13:40 +0000

    Rock Paper Scissors challenge solution

--- RPS.py
+++ RPS.py
@@ -1,10 +1,48 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# RPS.py
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+import random
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+def player(prev_play, opponent_history=[], my_history=[]):
+    # Track opponent's history
+    if prev_play:
+        opponent_history.append(prev_play)
+
+    # If no history yet, start with random
+    if not opponent_history:
+        guess = random.choice(["R", "P", "S"])
+    else:
+        # Count frequencies of opponent moves
+        counts = {"R": opponent_history.count("R"),
+                  "P": opponent_history.count("P"),
+                  "S": opponent_history.count("S")}
+
+        # Assume opponent will play their most frequent move
+        prediction = max(counts, key=counts.get)
+
+        # Counter strategy: play the winning move
+        beats = {"R": "P", "P": "S", "S": "R"}
+        guess = beats[prediction]
+
+        # --- Simple pattern detection ---
+        if len(opponent_history) >= 3:
+            last3 = "".join(opponent_history[-3:])
+            patterns = {}
+            for i in range(len(opponent_history)-3):
+                seq = "".join(opponent_history[i:i+3])
+                if seq == last3 and i+3 < len(opponent_history):
+                    nxt = opponent_history[i+3]
+                    patterns[nxt] = patterns.get(nxt, 0) + 1
+            if patterns:
+                prediction = max(patterns, key=patterns.get)
+                guess = beats[prediction]
+
+    # Save my move
+    if len(my_history) < len(opponent_history):
+        my_history.append(guess)
+    else:
+        if my_history:
+            my_history[-1] = guess
+        else:
+            my_history.append(guess)
 
     return guess
